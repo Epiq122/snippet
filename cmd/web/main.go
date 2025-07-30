7e936f0dd8ed34472d17472b695c8fbc334c094c
Robert Gleason <l2ob122@gmail.com>
2025-07-29 20:40:22 -0700

    feat: refactor main function to use routes method for handling HTTP requests

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"log"
 	"log/slog"
 	"net/http"
 	"os"
@@ -20,31 +19,13 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	// Initialize a new instance of our application struct, containing the
-	// dependencies (for now, just the structured logger).
 	app := &application{
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
-	// Use the Info() method to log the starting server message at Info severity
-	// (along with the listen address as an attribute).
 	logger.Info("starting server", "addr", *addr)
 
-	log.Printf("starting server on %s", *addr)
-
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
