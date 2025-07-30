d2b49491d7c00cccab07c6859cb0c993d8dcc302
Robert Gleason <l2ob122@gmail.com>
2025-07-29 19:00:42 -0700

    refactor home and snippetView handlers for clarity and consistency

--- main.go
+++ main.go
@@ -7,31 +7,30 @@ import (
 	"strconv"
 )
 
-// define a home handler function, which writes a byte slice
 func home(w http.ResponseWriter, r *http.Request) {
+	// Use the Header().Add() method to add a 'Server: Go' header to the
+	// response header map. The first parameter is the header name, and
+	// the second parameter is the header value.
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("Hello from snippetbox"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id wildcard from the request using r.PathValue()
-	// and try to convert it to an integer using the strconv.Atoi() function. If
-	// it can't be converted to an integer, or the value is less than 1, we
-	// return a 404 page not found response.
-
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
-
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Displaying a sp ecific snippet with ID %d...", id)
 }
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create a snippet"))
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// send a 201 status code.
+	w.WriteHeader(http.StatusCreated)
+
 	w.Write([]byte("save a new snippet"))
 }
 
