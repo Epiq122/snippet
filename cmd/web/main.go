01e7dce9612676061b1d1797c522c639743dfa7a
Robert Gleason <l2ob122@gmail.com>
2025-07-31 21:50:36 -0700

    feat: implement snippet creation and database insertion logic

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"github.com/joho/godotenv"
 	"log/slog"
 	"net/http"
 	"os"
@@ -19,36 +20,47 @@ type application struct {
 }
 
 func main() {
+	// Load .env file if it exists (optional, but useful)
+	err := godotenv.Load()
+	if err != nil {
+		// It’s okay if no .env file is present; just log it
+		// You can comment this line if you don't want the log
+		slog.Info("No .env file found, continuing...")
+	}
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
-	dsn := flag.String("dsn", os.Getenv("MYSQL_DSN"), "MySQL data source name")
+	dsn := flag.String("dsn", "", "MySQL data source name (overrides environment)")
 
 	flag.Parse()
 
+	if *dsn == "" {
+		*dsn = os.Getenv("MYSQL_DSN")
+	}
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	if *dsn == "" {
+		logger.Error("No DSN provided. Set MYSQL_DSN or pass -dsn flag")
+		os.Exit(1)
+	}
 
 	db, err := openDB(*dsn)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
-
 	defer db.Close()
 
-	// Initialize a models.SnippetModel instance containing the connection pool
-	// and add it to the application dependencies.
 	app := &application{
 		logger:   logger,
 		snippets: &models.SnippetModel{DB: db},
 	}
 
-	logger.Info("starting server", "addr", *addr)
+	logger.Info("Starting server", "addr", *addr)
 
 	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
-
 }
 
 func openDB(dsn string) (*sql.DB, error) {
